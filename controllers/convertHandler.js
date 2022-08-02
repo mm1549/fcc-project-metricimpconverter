44eb04019e5ec58ffafccd8e3d2c3c8290942c45
Replit user <>
2022-08-02 16:52:24 +0000

    Initial Commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,25 +1,72 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
+    let result = eval(input.match(/[\d./]*/)[0]);
+
     
+      
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
+    let result = input.match(/[a-zA-Z]*/)[0].toLowerCase();
     
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     let result;
+    switch(initUnit) {
+      case 'gal':
+        result = 'L';
+        break;
+      case 'L':
+        result = 'gal';
+        break;
+      case 'mi':
+        result = 'km';
+        break;
+      case 'km':
+        result = 'mi';
+        break;
+      case 'lbs':
+        result = 'kg';
+        break;
+      case 'kg':
+        result = 'lbs';
+        break;
+      default:
+        result = null;
+    }
     
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
+
+    switch(initUnit) {
+      case 'gal':
+        result = 'gallons';
+        break;
+      case 'L':
+        result = 'litres';
+        break;
+      case 'mi':
+        result = 'miles';
+        break;
+      case 'km':
+        result = 'kilometers';
+        break;
+      case 'lbs':
+        result = 'pounds';
+        break;
+      case 'kg':
+        result = 'kilograms';
+        break;
+      default:
+        result = null;
+    }
     
     return result;
   };
